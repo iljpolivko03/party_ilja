64021c0a47d33b2468c1b2425963e33ea28c7fa0
dfawess <31991330+dfawess@users.noreply.github.com>
2018-05-18 14:55:56 +0300

    jghj

--- connection.php
+++ connection.php
@@ -1,4 +1,7 @@
 <?php
+//$db=new
+//\atk4\data\Persistence_SQL('mysql:dbname=friends;host=localhost','root','');
+
 $db=new
 \atk4\data\Persistence_SQL('mysql:dbname=heroku_35a63eb77fee245;host=eu-mm-auto-fra-02-c.cleardb.net','bfa11752c77b7dt','77c43c35');
 class Friends extends \atk4\data\Model {
